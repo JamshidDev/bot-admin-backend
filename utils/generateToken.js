67b604cd06f9e209a8893487beb2d240fb2f60c5
JamshidDev <raximovj662@gmail.com>
2023-10-15 19:29:55 +0500

    fixed bugs

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,11 @@
 const JWT = require("jsonwebtoken")
 
+require("dotenv").config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const generateToken = (id)=>{
-    let token = JWT.sign({_id:id}, "Maxviy@Key");
+    let token = JWT.sign({_id:id}, SECRET_KEY);
     return token;
 }
 
