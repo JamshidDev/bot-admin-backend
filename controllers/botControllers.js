d6883cc1a5271228e99e64e8a33064e961988d84
JamshidDev <raximovj662@gmail.com>
2023-10-18 22:35:56 +0500

    added bot member route

--- controllers/botControllers.js
+++ controllers/botControllers.js
@@ -3,32 +3,29 @@ const MEMBER = require("../models/memberModels");
 const create_member = async (req, res) => {
   try {
     let { fullName, user_id, username, referal_id } = req.body;
-    console.log(req.body);
     let existUser = await MEMBER.findOne({ user_id });
-    console.log(existUser);
 
     if (!existUser) {
       let offer_user = await MEMBER.findOne({ user_id: referal_id });
-      console.log(offer_user);
-        if (offer_user) {
-          await MEMBER.findByIdAndUpdate(offer_user._id, {
-            invited_users: offer_user.invited_users + 1,
-          });
-        }
-        let member = await MEMBER.create({
-          fullName,
-          user_id,
-          username,
-          referal_id,
+      if (offer_user) {
+        await MEMBER.findByIdAndUpdate(offer_user._id, {
+          invited_users: offer_user.invited_users + 1,
         });
+      }
+      let member = await MEMBER.create({
+        fullName,
+        user_id,
+        username,
+        referal_id,
+      });
 
-        console.log(member);
+      console.log(member);
 
-        return res.status(200).json({
-          status: true,
-          message: "Muvofaqiyatli ro'yhatga olindi!",
-        });
-      
+      return res.status(200).json({
+        status: true,
+        message: "Muvofaqiyatli ro'yhatga olindi!",
+      });
+
     } else {
       await MEMBER.findByIdAndUpdate(existUser._id, {
         fullName,
@@ -36,7 +33,6 @@ const create_member = async (req, res) => {
         username,
         active: true,
       });
-      console.log("no");
       return res.status(200).json({
         status: false,
         message: "Foydalanuvchi allaqachon ro'yhatdan o'tgan!",
@@ -51,4 +47,81 @@ const create_member = async (req, res) => {
   }
 };
 
-module.exports = { create_member };
+
+const remove_user = async (req, res) => {
+  try {
+    let user_id = req.params.user_id;
+    let existUser = await MEMBER.findOne({ user_id });
+    if (existUser) {
+
+      // if(existUser.referal_id){
+      //   let inviter_user = await MEMBER.findOne({ user_id: +existUser.referal_id }); 
+      //   await MEMBER.findByIdAndUpdate(inviter_user._id, {
+      //     invited_users: inviter_user.invited_users - 1,
+      //   });
+      // }
+      await MEMBER.findByIdAndUpdate(existUser._id, {
+        active: false,
+      });
+      return res.status(200).json({
+        status: true,
+        message: "Foydalanuvchi muvofaqiyatli ravishda o'chirildi!",
+      });
+
+    } else {
+      return res.status(200).json({
+        status: false,
+        message: "user_id ga mos foydalanuvchi ro'yhatdan topilmadi!",
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+}
+
+
+const get_all_details = async (req, res) => {
+  try {
+    let all_active_users = await MEMBER.find({ active: true })
+    return res.status(200).json({
+      status: true,
+      data: all_active_users,
+      message: null,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+}
+
+
+const user_statistic = async (req, res) => {
+  try {
+    let active_count = await MEMBER.countDocuments({ active: true })
+    let count = await MEMBER.countDocuments()
+    return res.status(200).json({
+      status: true,
+      data: {
+        all_users_count: count,
+        active_users_count: active_count,
+        inactive_users_count: count - active_count,
+      },
+      message: null,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+}
+
+module.exports = { create_member, remove_user, get_all_details, user_statistic };
--- router/botRouter.js
+++ router/botRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {create_member} = require("../controllers/botControllers")
+const {create_member, remove_user,get_all_details, user_statistic} = require("../controllers/botControllers")
 
 
-// router.get("/", get_bank)
+router.get("/member", get_all_details)
+router.get("/member/statistic", user_statistic)
 router.post("/member/create", create_member)
 // router.put("/update/:bank_id", update_bank)
-// router.delete("/delete/:bank_id", delete_bank)
+router.delete("/member/delete/:user_id", remove_user)
 
 
 
